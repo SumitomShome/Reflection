f752e32ae40954a5061eb51183191075647a113c
SumitomShome <sumitomshome@gmail.com>
2020-11-29 12:06:12 +0530

    [Sumitom] Add: Method to use reflection

---
+++ ReflectionDemoProjectLive/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+namespace ReflectionProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Reflection demo live project");
+           ReflectionOnCustomer.ReflectionTest();
+           Console.ReadKey();
+        }
+    }
+}
